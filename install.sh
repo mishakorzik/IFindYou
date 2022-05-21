3a740736038b4ab74039835949cde4f90de62587
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-05-21 13:41:27 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+wget https://bin.equinox.io/a/e93TBaoFgZw/ngrok-2.2.8-linux-arm.zip > /dev/null 2>&1
+if [[ -e ngrok-2.2.8-linux-arm.zip ]]; then
+unzip ngrok-2.2.8-linux-arm.zip > /dev/null 2>&1
+rm -rf $HOME/.ngrok2 > /dev/null 2>&1
+chmod +x ngrok
+rm -rf ngrok-2.2.8-linux-arm.zip
+fi
