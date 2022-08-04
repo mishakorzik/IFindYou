082361e3c65afa777c2db209b1b2ab60957566d5
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-08-04 10:55:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm.tgz > /dev/null 2>&1
 if [[ -e ngrok-v3-stable-linux-arm.tgz ]]; then
-untar -xf ngrok-v3-stable-linux-arm.tgz  > /dev/null 2>&1
+tar -xf ngrok-v3-stable-linux-arm.tgz  > /dev/null 2>&1
 rm -rf $HOME/.ngrok2 > /dev/null 2>&1
 chmod +x ngrok
 rm -rf ngrok-v3-stable-linux-arm.tgz
